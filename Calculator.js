b4a50705416cee74b42b04f7da7b3814111ed8e9
EthanFriedrichs <ethanpfriedrichs3@gmail.com>
2023-10-12 08:19:42 -0400

    Comitted back my functions

--- Calculator.js
+++ Calculator.js
@@ -5,7 +5,6 @@ function addThing(x)
 {
     if (flag == 0) // Decides whether to make a number bigger or to add a new number
     {
-    //console.log(x.innerHTML, "Added numb to array", flag)
     numbArray.push(x.innerHTML)
     console.log(numbArray)
     flag = 1
@@ -13,7 +12,6 @@ function addThing(x)
 
     else if (!(isNaN(Number(x.innerHTML))))
     {
-        //console.log(numbArray.length - 1, "Merging numbers")
         numbArray[numbArray.length - 1] = String(numbArray[numbArray.length - 1]) + String(x.innerHTML)
         console.log(numbArray)
     }
@@ -21,7 +19,6 @@ function addThing(x)
 
 function addSymbol(x) // Gets a symbol buttons symbol and adds it to the array (sets flag to 1)
 {
-    //console.log(x.innerHTML, "Pushed symbol", flag)
     numbArray.push(x.innerHTML)
     console.log(numbArray)
     flag = 0
