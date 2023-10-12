82f38fd9b006fab6222508a42527f64880a9f2d9
Kishaun13 <Kishaunbrown14@gmail.com>
2023-10-12 10:11:16 -0400

    formatted code

--- Calculator.js
+++ Calculator.js
@@ -1,42 +1,25 @@
 var numbArray = [];
 var flag = 0;
 
-
-function addThing(x) 
-{
-    if (numbArray.length == 1 && numbArray[numbArray.length - 1] === "-") 
-    {
-        // If previous element is - than append the number to it
-        numbArray[numbArray.length - 1] = numbArray[numbArray.length - 1] + String(x.innerHTML);
-        console.log(numbArray);
-        flag = 1;
-    }
-  
-    else if (flag == 0) 
-    {
+function addThing(x) {
+  if (flag == 0) {
     // Decides whether to make a number bigger or to add a new number
     console.log(x.innerHTML, "Added numb to array");
     numbArray.push(x.innerHTML);
     console.log(numbArray);
     flag = 1;
-    } 
-  
-    else 
-    {
+  } else {
     console.log(numbArray.length - 1, "Merging numbers", x.innerHTML);
     numbArray[numbArray.length - 1] =
       String(numbArray[numbArray.length - 1]) + String(x.innerHTML);
     console.log(numbArray);
-    }
-
+  }
   updateDisplay(numbArray.join(" "));
 }
-
 
 function addSymbol(x) {
   flag = 0;
   numbArray.push(x.innerHTML);
-
 
   // If the symbol is "=", calculate the result
   if (x.innerHTML === "=") {
@@ -49,16 +32,13 @@ function addSymbol(x) {
   }
 }
 
-
 // Calculates the result of the numbArray and returns it
 function calculateResult() {
   let result = 0;
   let currentOperator = "";
 
-
   for (let i = 0; i < numbArray.length; i++) {
     let currentElement = numbArray[i];
-
 
     if (!isNaN(currentElement)) {
       // If the current element is a number
