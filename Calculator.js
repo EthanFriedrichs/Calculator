9134abae761c2dd7d3d3bc3e4627c080de94ae60
EthanFriedrichs <ethanpfriedrichs3@gmail.com>
2023-10-12 07:58:06 -0400

    Finished functions to add numbers into the array

--- Calculator.js
+++ Calculator.js
@@ -5,15 +5,15 @@ function addThing(x)
 {
     if (flag == 0) // Decides whether to make a number bigger or to add a new number
     {
-    console.log(x.innerHTML, "Added numb to array", )
+    //console.log(x.innerHTML, "Added numb to array", flag)
     numbArray.push(x.innerHTML)
     console.log(numbArray)
     flag = 1
     }
 
-    else
+    else if (!(isNaN(Number(x.innerHTML))))
     {
-        console.log(numbArray.length - 1, "Merging numbers", x.innerHTML)
+        //console.log(numbArray.length - 1, "Merging numbers")
         numbArray[numbArray.length - 1] = String(numbArray[numbArray.length - 1]) + String(x.innerHTML)
         console.log(numbArray)
     }
@@ -21,8 +21,8 @@ function addThing(x)
 
 function addSymbol(x) // Gets a symbol buttons symbol and adds it to the array (sets flag to 1)
 {
-    flag = 1
-    console.log(x.innerHTML, "Pushed symbol")
+    //console.log(x.innerHTML, "Pushed symbol", flag)
     numbArray.push(x.innerHTML)
     console.log(numbArray)
+    flag = 0
 }
